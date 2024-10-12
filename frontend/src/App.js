59bca0696bfcd540ccc81b9fa8038ae72e662be6
safia-safdar <safia.n.safdar@gmail.com>
2024-10-12 19:22:59 +0100

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Unity Fund</h1>
+        <p>
+          Muslim lending platform
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
